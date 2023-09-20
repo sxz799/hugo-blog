e47aaa9eb083b75b36cd4fc87b86314079aa4a35
sxz799 <sxz799@126.com>
2023-09-20 22:56:59 +0800

    update blog

--- push.sh
+++ push.sh
@@ -5,6 +5,8 @@ hugo --gc #编译出新文件
 
 echo '5ecc25eeb0ed65ac0edac01cb9e724bb' > public/baidu_verify_codeva-V7JhMVJzw4.html ## 这个是百度的校验文件
 
+zip -r public.zip public
+
 git add .
 
 git commit -m "update blog"
