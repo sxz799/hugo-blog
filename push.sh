d972a18916f85c6d3ab39d41e9384d02ca25f64d
sxz799 <sxz799@126.com>
2023-10-06 23:58:14 +0800

    update blog

--- push.sh
+++ push.sh
@@ -1,7 +1,8 @@
 rm -rf docs/ # 删除旧的文件
 
 hugo --gc #编译出新文件
-# echo 'blog.sxz799.xyz' > public/CNAME ## 如果配置的域名就加上这个
+
+echo 'blog.sxz799.xyz' > public/CNAME ## 如果配置的域名就加上这个
 
 echo '5ecc25eeb0ed65ac0edac01cb9e724bb' > public/baidu_verify_codeva-V7JhMVJzw4.html ## 这个是百度的校验文件
 
